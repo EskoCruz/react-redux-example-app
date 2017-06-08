1f9f8618251f92fb789472b1ee43c60d7ca0d00e
Esko Cruzeta <ecruz@tagrem.com>
2017-06-07 17:06:20 -0700

    Displaying the State

--- src/index.js
+++ src/index.js
@@ -1,14 +1,45 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import {createStore} from 'redux';
+
+
+const defaultState = { checked: false };
+const reducer = function (state = defaultState, action) {
+    switch (action.type) {
+        case 'TOGGLE':
+            return {...state, checked: !state.checked};
+    }
+    return state;
+};
+
+const store = createStore(reducer);
 
 class App extends React.Component {
-  render() {
-    return (
-      <h1>Hello, World!</h1>
-    );
-  }
+    constructor() {
+        super();
+        this.state = {};
+    }
+
+    componentWillMount() {
+        store.subscribe(() => this.setState(store.getState()));
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>To-dos!</h1>
+                <div>
+                    Learn Redux&nbsp;
+                    <input type="checkbox" checked="{!!this.state.checked}" />
+                </div>
+                {
+                    this.state.checked ? (<h2>Done!</h2>) : null
+                }
+            </div>
+        );
+    }
 }
 
 ReactDOM.render((
-  <App />
+    <App />
 ), document.getElementById('root'));
